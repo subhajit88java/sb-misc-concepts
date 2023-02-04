f54e58ed7d185e534d10bca07a20a323bdcbc4b5
Subhajit Kar <subhajitt88@gmail.com>
2023-02-05 00:47:56 +0530

    commit
    commit

---
+++ src/main/java/com/subhajit/sbmiscconcepts/bcrypt/controller/BcryptController.java
@@ -0,0 +1,20 @@
+package com.subhajit.sbmiscconcepts.bcrypt.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BcryptController {
+	
+	@GetMapping("/bcrypt-encode")
+	public String bcryptEncode() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String password = "a";
+		String encodedPassword = passwordEncoder.encode(password);
+		System.out.println("Encoded password : " + encodedPassword);
+		Boolean flag = passwordEncoder.matches("a", encodedPassword);
+		return encodedPassword;
+	}
+
+}
